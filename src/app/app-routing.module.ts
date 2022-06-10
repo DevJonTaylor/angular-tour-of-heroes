3bffa688d8c024a54ae17a020f86c05a13db2742
Jon Taylor <jonnytest1101@icloud.com>
2022-06-10 13:42:27 -0500

    Completed
    Reworked Hero Details to be a page. Removed Hero Details from Heroes.  Setup styling for App, Navbar, Heroes, Dashboard, and Hero Details.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router'
 import { HeroesComponent } from './heroes/heroes.component'
 import { NotFoundComponent } from './not-found/not-found.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'
 
 const routes: Routes = [
+  { path: '', redirectTo: 'dash', pathMatch: 'full' },
+
   { path: 'heroes', component: HeroesComponent },
   { path: 'dash', component: DashboardComponent },
-
-  { path: '', redirectTo: 'dash', pathMatch: 'full' },
+  { path: 'hero/:id', component: HeroDetailComponent },
 
   // Must be last! 404
   { path: '**', component: NotFoundComponent },
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,7 +12,6 @@ import { MessageService } from '../message.service'
 export class HeroesComponent implements OnInit {
 
   heroes: Array<Hero> = []
-  selectedHero?: Hero
 
   constructor(private messageService: MessageService, private heroService: HeroService) {}
 
@@ -25,15 +24,5 @@ export class HeroesComponent implements OnInit {
       .heroService
       .getHeroes()
       .subscribe(heroes => this.heroes = heroes)
-  }
-
-  onSelect(hero: Hero): void {
-    if(hero === this.selectedHero || !hero) {
-      this.selectedHero = null
-      return
-    }
-
-    this.selectedHero = hero
-    this.messageService.add(`HeroComponent: Selected hero id=${hero.id}`)
   }
 }
